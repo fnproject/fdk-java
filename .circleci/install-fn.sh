55f1519e5d4660f4af44085aa2738dd77c8d7f19
Will Price <will.price94@gmail.com>
2017-08-25 11:11:07 +0100

    Update fn cli references to new location

--- .circleci/install-fn.sh
+++ .circleci/install-fn.sh
@@ -3,13 +3,13 @@
 set -ex
 
 export GOPATH=/tmp/go
-FN_SRC_DIR="$GOPATH/src/github.com/fnproject/fn"
+FN_SRC_DIR="$GOPATH/src/github.com/fnproject/cli"
 FN_INSTALL_PATH=/usr/local/bin/fn
 
 mkdir -p "$GOPATH"
 
 if [ ! -d "$FN_SRC_DIR" ]; then
-    git clone git@github.com:fnproject/fn.git "$FN_SRC_DIR"
+    git clone git@github.com:fnproject/cli.git "$FN_SRC_DIR"
 else
     pushd "$FN_SRC_DIR"
         git reset --hard
@@ -19,7 +19,8 @@ else
     popd
 fi
 
-pushd "${FN_SRC_DIR}/cli"
+pushd "${FN_SRC_DIR}"
+    glide install -v
     go build -o fn
     sudo cp fn "$FN_INSTALL_PATH"
 popd
