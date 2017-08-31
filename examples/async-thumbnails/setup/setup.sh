ce0b5ef7b110bbef25197dcf509eb401017cf67c
Will Price <will.price@oracle.com>
2017-08-31 10:17:43 +0100

    async-thumbnails: Improve setup/setup.sh
    * Split docker run commands over multiple lines for easier reading
    * Point user to download links for minio client
    * Pull docker images before running

--- examples/async-thumbnails/setup/setup.sh
+++ examples/async-thumbnails/setup/setup.sh
@@ -3,6 +3,10 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 STORAGE_DIR="/tmp/example-storage-server-files"
 
+COMPLETER_IMAGE=fnproject/completer
+FUNCTIONS_IMAGE=fnproject/functions
+MINIO_IMAGE=minio/minio
+
 # Checks
 if docker ps &>/dev/null ; then
     echo "Docker is present."
@@ -19,13 +23,40 @@ fi
 if type mc &>/dev/null ; then
     echo "mc is present."
 else
-    echo "error: mc is not available, please install it from the minio client website"
+    echo "error: mc is not available, please install it from https://github.com/minio/mc"
+    echo "mac os: brew install minio/stable/mc"
+    echo "linux: sudo curl -sL https://dl.minio.io/client/mc/release/linux-amd64/mc -o /usr/local/bin/mc && sudo chmod +x /usr/local/bin/mc"
     exit 1
 fi
 
+docker pull "$COMPLETER_IMAGE"
+docker pull "$FUNCTIONS_IMAGE"
+docker pull "$MINIO_IMAGE"
+
+# Set up the storage server and upload directory
+if [[ -z `docker ps | grep "example-storage-server"` ]]; then
+    mkdir -p "$STORAGE_DIR"
+    docker run -d --name example-storage-server \
+        -e "MINIO_ACCESS_KEY=alpha" \
+        -e "MINIO_SECRET_KEY=betabetabetabeta" \
+        -v "$STORAGE_DIR":/export \
+        -p 9000:9000 \
+        "$MINIO_IMAGE" server /export
+    # Give it time to start up
+    sleep 3
+else
+    echo "Storage server is already up."
+fi
+
+STORAGE_SERVER_IP=`docker inspect --type container -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' example-storage-server`
+
 # Start functions server if not there
 if [[ -z `docker ps | grep "functions"` ]]; then
-    docker run -d --name functions -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock fnproject/functions:latest
+    docker run -d --name functions \
+        -e NO_PROXY="$STORAGE_SERVER_IP:$NO_PROXY" \
+        -p 8080:8080 \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        "$FUNCTIONS_IMAGE"
     # Give it time to start up
     sleep 3
 else
@@ -36,7 +67,12 @@ FUNCTIONS_SERVER_IP=`docker inspect --type container -f '{{range .NetworkSetting
 
 # Start cloud-completer if not there
 if [[ -z `docker ps | grep "cloud-completer"` ]]; then
-    docker run -d --name cloud-completer -p 8081:8081 --env API_URL=http://$FUNCTIONS_SERVER_IP:8080/r fnproject/completer:latest
+    docker run -d --name cloud-completer \
+        -e LOG_LEVEL=debug \
+        -e NO_PROXY="$FUNCTIONS_SERVER_IP:$NO_PROXY" \
+        -e API_URL=http://$FUNCTIONS_SERVER_IP:8080/r \
+        -p 8081:8081 \
+        "$COMPLETER_IMAGE"
     # Give it time to start up
     sleep 3
 else
@@ -50,7 +86,7 @@ if [[ `fn apps list` == *"myapp"* ]]; then
     echo "App myapp is already there."
 else
     fn apps create myapp
-    fn apps config set myapp COMPLETER_BASE_URL http://${COMPLETER_SERVER_IP}:8081
+    fn apps config set myapp COMPLETER_BASE_URL http://10.167.103.193:8081
 fi
 
 if [[ `fn routes list myapp` == *"/resize128"* ]]; then
@@ -58,7 +94,10 @@ if [[ `fn routes list myapp` == *"/resize128"* ]]; then
 else
     # This works around proxy issues
     cd $SCRIPT_DIR/resize128 && \
-        docker build -t example/resize128:0.0.1 --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy . && \
+        docker build -t example/resize128:0.0.1 \
+            --build-arg http_proxy=$http_proxy \
+            --build-arg https_proxy=$https_proxy \
+            . && \
         fn routes create myapp /resize128
 fi
 if [[ `fn routes list myapp` == *"/resize256"* ]]; then
@@ -66,7 +105,10 @@ if [[ `fn routes list myapp` == *"/resize256"* ]]; then
 else
     # This works around proxy issues
     cd $SCRIPT_DIR/resize256 && \
-        docker build -t example/resize256:0.0.1 --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy . && \
+        docker build -t example/resize256:0.0.1 \
+            --build-arg http_proxy=$http_proxy \
+            --build-arg https_proxy=$https_proxy \
+            . && \
         fn routes create myapp /resize256
 fi
 if [[ `fn routes list myapp` == *"/resize512"* ]]; then
@@ -74,19 +116,13 @@ if [[ `fn routes list myapp` == *"/resize512"* ]]; then
 else
     # This works around proxy issues
     cd $SCRIPT_DIR/resize512 && \
-        docker build -t example/resize512:0.0.1 --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy . && \
+        docker build -t example/resize512:0.0.1 \
+            --build-arg http_proxy=$http_proxy \
+            --build-arg https_proxy=$https_proxy \
+            . && \
         fn routes create myapp /resize512
 fi
 
-# Set up the storage server and upload directory
-if [[ -z `docker ps | grep "example-storage-server"` ]]; then
-    mkdir -p "$STORAGE_DIR"
-    docker run -d -e "MINIO_ACCESS_KEY=alpha" -e "MINIO_SECRET_KEY=betabetabetabeta" -v "$STORAGE_DIR":/export --name example-storage-server -p 9000:9000 minio/minio server /export
-    # Give it time to start up
-    sleep 3
-else
-    echo "Storage server is already up."
-fi
 
 if mc config host list | grep example-storage-server &>/dev/null ; then
     echo "mc example-storage-server configuration is already present"
