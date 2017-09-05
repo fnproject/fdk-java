bb7af967904eb60e9cb169f7212e7ed6babe51c0
Dario Domizioli <dario.domizioli@gmail.com>
2017-09-05 13:59:52 +0100

    Make it clear that in the real case the class loader param is ignored

--- runtime/src/main/java/com/fnproject/fn/runtime/cloudthreads/CloudCompleterApiClient.java
+++ runtime/src/main/java/com/fnproject/fn/runtime/cloudthreads/CloudCompleterApiClient.java
@@ -198,7 +198,7 @@ public class CloudCompleterApiClient implements CompleterClient {
 
     // wait for completion  -> result
     @Override
-    public Object waitForCompletion(ThreadId threadId, CompletionId id, ClassLoader loader) {
+    public Object waitForCompletion(ThreadId threadId, CompletionId id, ClassLoader ignored) {
         while (true) {
             long time = System.currentTimeMillis();
             try (HttpClient.HttpResponse response = httpClient.execute(HttpClient
