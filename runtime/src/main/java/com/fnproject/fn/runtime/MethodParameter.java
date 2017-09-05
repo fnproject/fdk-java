f510731fb13f55e2db1fa187ea2aa3f94e8ac683
Will Price <will.price94@gmail.com>
2017-09-05 23:20:53 +0100

    Reduce visibility of new Method* classes

--- runtime/src/main/java/com/fnproject/fn/runtime/MethodParameter.java
+++ runtime/src/main/java/com/fnproject/fn/runtime/MethodParameter.java
@@ -2,10 +2,10 @@ package com.fnproject.fn.runtime;
 
 import com.fnproject.fn.api.MethodWrapper;
 
-public class MethodParameter extends DefaultMethodType {
+class MethodParameter extends DefaultMethodType {
     private int index;
 
-    public MethodParameter(MethodWrapper src, int index) {
+    MethodParameter(MethodWrapper src, int index) {
         super(src, resolveType(src.getTargetMethod().getGenericParameterTypes()[index], src));
         this.index = index;
     }
