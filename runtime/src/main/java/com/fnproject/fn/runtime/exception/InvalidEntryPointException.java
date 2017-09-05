ad089322827e494aa89f0c761b349b3591de1b7a
Will Price <will.price94@gmail.com>
2017-09-05 22:41:37 +0100

    Remove Spring references

--- runtime/src/main/java/com/fnproject/fn/runtime/exception/InvalidEntryPointException.java
+++ runtime/src/main/java/com/fnproject/fn/runtime/exception/InvalidEntryPointException.java
@@ -1,7 +1,7 @@
 package com.fnproject.fn.runtime.exception;
 
-import org.springframework.beans.BeansException; /**
- * The function entrypoint was malformed .
+/**
+ * The function entrypoint was malformed.
  */
 public class InvalidEntryPointException extends FunctionLoadException {
     public InvalidEntryPointException(String msg) {
