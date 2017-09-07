b69ba213092a97d35649619eead40ed22c37bef3
gviedma <g.viedma@yieldlab.de>
2017-09-07 14:26:06 +0100

    added missing import

--- integration-tests/main/test-6/src/main/java/com/fnproject/fn/integration/test_6/CompleterFunction.java
+++ integration-tests/main/test-6/src/main/java/com/fnproject/fn/integration/test_6/CompleterFunction.java
@@ -2,19 +2,22 @@ package com.fnproject.fn.integration.test_6;
 
 import com.fnproject.fn.api.cloudthreads.CloudThreadRuntime;
 import com.fnproject.fn.api.cloudthreads.CloudThreads;
+
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class CompleterFunction {
 
     public Integer handleRequest(String input) {
         CloudThreadRuntime rt = CloudThreads.currentRuntime();
         try {
-            return rt.supply(() -> { Thread.sleep(10000); return 42; }).get();
-                    //.get(1000, TimeUnit.MILLISECONDS);
-        } catch(TimeoutException t) {
+            return rt.supply(() -> {
+                Thread.sleep(10000);
+                return 42;
+            }).get(1000, TimeUnit.MILLISECONDS);
+        } catch (TimeoutException t) {
             System.err.println("Caught timeout");
-            // return 20;
-            return 42;
+            return 20;
         }
     }
 
