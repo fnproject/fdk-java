9bf71dc444a6e4a393bf94a3e774475b5f51119f
Dario Domizioli <dario.domizioli@gmail.com>
2017-09-19 16:04:23 +0100

    Second attempt at including the test deps; this one worked

--- build-image/cache-deps.sh
+++ build-image/cache-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /tmp/cache-deps && mvn package dependency:copy-dependencies -Dmaven.repo.local=/usr/share/maven/ref/repository -DskipTests=true -Dmdep.prependGroupId=true -DoutputDirectory=target --fail-never
+cd /tmp/cache-deps && mvn test package dependency:copy-dependencies -DproxyHost=www-proxy.uk.oracle.com -DproxyPort=80 -Dmaven.repo.local=/usr/share/maven/ref/repository -Dmdep.prependGroupId=true -DoutputDirectory=target --fail-never
 cd / && rm -fr /tmp/cache-deps
---
+++ build-image/src/test/java/com/example/fn/BuildImageTest.java
@@ -0,0 +1,14 @@
+package com.example.fn;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class BuildImageTest {
+
+    @Test
+    public void mustPass() {
+        assertEquals("a", "a");
+    }
+
+}
