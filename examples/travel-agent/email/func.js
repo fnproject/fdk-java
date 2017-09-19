19495616f6a84e4c9f298466707cdf896b21b3e4
Thom Leggett <thom@tteggel.org>
2017-09-19 03:27:38 +0100

    Add email function

---
+++ examples/travel-agent/email/func.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var fs = require('fs');
+
+var api_url = process.env.EMAIL_API_URL;
+var stdin = JSON.parse(fs.readFileSync('/dev/stdin').toString());
+
+request.post(
+    api_url,
+    {json: stdin},
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(JSON.stringify(response.body));
+        }
+    }
+);
