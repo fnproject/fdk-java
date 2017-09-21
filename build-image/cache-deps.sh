9a17144b6c77a5a9f81e4bc629949458f7a1dd68
Matthew Gilliard <matthew.gilliard@oracle.com>
2017-09-21 12:39:57 +0100

    Fail the build if maven fails in the build image

--- build-image/cache-deps.sh
+++ build-image/cache-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /tmp/cache-deps && mvn test package dependency:copy-dependencies -Dmaven.repo.local=/usr/share/maven/ref/repository -Dmdep.prependGroupId=true -DoutputDirectory=target --fail-never
+cd /tmp/cache-deps && mvn test package dependency:copy-dependencies -Dmaven.repo.local=/usr/share/maven/ref/repository -Dmdep.prependGroupId=true -DoutputDirectory=target
 cd / && rm -fr /tmp/cache-deps
