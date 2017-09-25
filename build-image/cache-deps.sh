076bc86fb458e56abc324ab9ba0885855d0de94f
jan grant <jang@ioctl.org>
2017-09-25 13:29:48 +0100

    Update the build-image script to run in a proxy environment
    Circle builds continue as before, but this is a step towards being
    able to replicate everything behind a proxy with somewhat less hassle.

--- build-image/cache-deps.sh
+++ build-image/cache-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
-cd /tmp/cache-deps && mvn test package dependency:copy-dependencies -Dmaven.repo.local=/usr/share/maven/ref/repository -Dmdep.prependGroupId=true -DoutputDirectory=target
+cd /tmp/cache-deps && mvn test package dependency:copy-dependencies -Dmaven.repo.local=/usr/share/maven/ref/repository -Dmdep.prependGroupId=true -DoutputDirectory=target --settings /tmp/settings.xml
 cd / && rm -fr /tmp/cache-deps
--- build-image/docker-build.sh
+++ build-image/docker-build.sh
@@ -1,7 +1,55 @@
 #!/bin/bash -ex
 
-cd /tmp/staging-repository && python -mSimpleHTTPServer 18080 1>>/tmp/http-logs 2>&1 &
-SRV_PROCESS=$!
+. "$(dirname "$0")"/../integration-tests/lib.sh
 
-docker build $*
-kill $SRV_PROCESS
+CID=$(docker run \
+          -d \
+          -v /tmp/staging-repository:/tmp/staging-repository \
+          -p 18080 \
+          python:2.7 \
+          /bin/sh -c '
+    set -e
+    cd /tmp/staging-repository
+    python -mSimpleHTTPServer 18080')
+
+defer docker rm -f "$CID"
+
+IP=$(docker inspect --type container "$CID" -f '{{ .NetworkSettings.IPAddress }}')
+
+T=$(mktemp -d)
+defer rm -rf "$T"
+
+(
+cp -R . "$T/"
+
+cd "$T"
+sed -i.bak -e "s/%LOCAL_SERVER%/$IP/" pom.xml
+
+# Fix up a settings.xml if required
+if [ -z "$http_proxy" ]; then
+  PROXY_ACTIVE=false
+  PROXY_PROTO=
+  PROXY_HOST=
+  PROXY_PORT=
+  PROXY_NOPROXY=
+else
+  PROXY_ACTIVE=true
+  PROXY_PROTO=${http_proxy%%:*}
+  PROXY_HOST=$(echo "$http_proxy" | sed -e 's|.*://||' -e 's|:.*||')
+  PROXY_PORT=$(echo "$http_proxy" | sed -e 's|.*://.*:||' -e 's|/$||')
+  PROXY_NOPROXY=$(echo "$no_proxy" |
+      tr ',' '\n' |
+      sed -e 's|^\.|*.|' -e 's|\.$|.*|' |
+      tr '\n' '|')
+fi
+
+sed -i.bak \
+    -e "s/%PROXY_ACTIVE%/$PROXY_ACTIVE/g" \
+    -e "s/%PROXY_PROTO%/$PROXY_PROTO/g" \
+    -e "s/%PROXY_HOST%/$PROXY_HOST/g" \
+    -e "s/%PROXY_PORT%/$PROXY_PORT/g" \
+    -e "s/%PROXY_NOPROXY%/$PROXY_NOPROXY/g" \
+    settings.xml
+
+docker build "$@"
+)
