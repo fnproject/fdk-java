b3fa77ac5c0e05582572e5ba950de6767ca96f58
Owen Cliffe <owen.cliffe@oracle.com>
2018-09-15 15:03:43 +0100

    fix test interference

--- runtime/src/test/java/com/fnproject/fn/runtime/HTTPStreamCodecTest.java
+++ runtime/src/test/java/com/fnproject/fn/runtime/HTTPStreamCodecTest.java
@@ -8,22 +8,21 @@ import org.apache.commons.io.IOUtils;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.api.Result;
+import org.eclipse.jetty.client.util.BytesContentProvider;
 import org.eclipse.jetty.client.util.StringContentProvider;
 import org.eclipse.jetty.unixsocket.client.HttpClientTransportOverUnixSockets;
 import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -32,20 +31,19 @@ import java.util.concurrent.atomic.AtomicReference;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
+ *
+ * This uses the Jetty client largely as witness of "good HTTP behaviour"
  * Created on 24/08/2018.
  * <p>
  * (c) 2018 Oracle Corporation
  */
 public class HTTPStreamCodecTest {
-    private static final String fileName;
 
-    private static final String FN_LISTENER;
 
     private static final Map<String, String> defaultEnv;
+    private final List<Runnable> cleanups = new ArrayList<>();
 
-    private HTTPStreamCodec codec;
-
-    private static File createSocketFile() {
+    private static File generateSocketFile() {
         File f = null;
         try {
 
@@ -60,25 +58,31 @@ public class HTTPStreamCodecTest {
 
     static {
         System.setProperty("com.fnproject.java.native.libdir", new File("src/main/c/").getAbsolutePath());
-
         System.setProperty("org.eclipse.jetty.util.log.class", "org.eclipse.jetty.util.log.StdErrLog");
         System.setProperty("org.eclipse.jetty.LEVEL", "WARN");
-        fileName = createSocketFile().getAbsolutePath();
-        FN_LISTENER = "unix:" + fileName;
-        httpClient = new HttpClient(new HttpClientTransportOverUnixSockets(fileName), null);
 
 
         Map<String, String> env = new HashMap<>();
         env.put("FN_APP_NAME", "myapp");
         env.put("FN_PATH", "mypath");
-        env.put("FN_LISTENER", FN_LISTENER);
 
         defaultEnv = Collections.unmodifiableMap(env);
     }
 
-    private static HttpClient httpClient;
+    private HttpClient createClient(File unixSocket) throws Exception {
+        HttpClient client = new HttpClient(new HttpClientTransportOverUnixSockets(unixSocket.getAbsolutePath()), null);
+        client.start();
+        cleanups.add(() -> {
+            try {
+                client.stop();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return client;
+    }
 
-    private Request defaultRequest() {
+    private Request defaultRequest(HttpClient httpClient) {
         return httpClient.newRequest("http://localhost/call")
           .method("POST")
           .header("Fn-Call-Id", "callID")
@@ -91,44 +95,42 @@ public class HTTPStreamCodecTest {
     }
 
 
-    @BeforeClass
-    public static void setup() throws Exception {
-        httpClient.start();
-    }
-
-    @AfterClass
-    public static void stop() throws Exception {
-        httpClient.stop();
-    }
-
     @After
     public void cleanup() throws Exception {
-        File listener = new File(fileName);
-        listener.delete();
-        if (codec != null) {
-            codec.close();
-            codec = null;
-        }
-
+        cleanups.forEach(Runnable::run);
     }
 
 
-    public void startCodec(Map<String, String> env, EventCodec.Handler h) {
-        codec = new HTTPStreamCodec(defaultEnv);
+    public File startCodec(Map<String, String> env, EventCodec.Handler h) {
+        Map<String, String> newEnv = new HashMap<>(env);
+        File socket = generateSocketFile();
+        newEnv.put("FN_LISTENER", "unix:" + socket.getAbsolutePath());
+
+        HTTPStreamCodec codec = new HTTPStreamCodec(newEnv);
+
         Thread t = new Thread(() -> codec.runCodec(h));
         t.start();
+        cleanups.add(() -> {
+            try {
+                codec.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return socket;
     }
 
     @Test
     public void shouldAcceptDataOnHttp() throws Exception {
         CompletableFuture<InputEvent> lastEvent = new CompletableFuture<>();
 
-        startCodec(defaultEnv, (in) -> {
+        File socketFile = startCodec(defaultEnv, (in) -> {
             lastEvent.complete(in);
             return OutputEvent.fromBytes("hello".getBytes(), OutputEvent.Status.Success, "text/plain", Headers.emptyHeaders().addHeader("x-test", "bar"));
         });
 
-        ContentResponse resp = httpClient.newRequest("http://localhost/call")
+        HttpClient client = createClient(socketFile);
+        ContentResponse resp = client.newRequest("http://localhost/call")
           .method("POST")
           .header("Fn-Call-Id", "callID")
           .header("Fn-Deadline", "2002-10-02T10:00:00.992Z")
@@ -139,14 +141,12 @@ public class HTTPStreamCodecTest {
 
         assertThat(resp.getStatus()).isEqualTo(200);
         assertThat(resp.getContent()).isEqualTo("hello".getBytes());
-        System.err.println("headers" + resp.getHeaders());
         assertThat(resp.getHeaders().get("x-test")).isEqualTo("bar");
 
         InputEvent evt = lastEvent.get(1, TimeUnit.MILLISECONDS);
         assertThat(evt.getCallID()).isEqualTo("callID");
         assertThat(evt.getDeadline().toEpochMilli()).isEqualTo(1033552800992L);
         assertThat(evt.getHeaders()).isEqualTo(Headers.emptyHeaders().addHeader("Fn-Call-Id", "callID").addHeader("Fn-Deadline", "2002-10-02T10:00:00.992Z").addHeader("Custom-header", "v1", "v2").addHeader("Content-Type", "text/plain"));
-
 
     }
 
@@ -156,7 +156,7 @@ public class HTTPStreamCodecTest {
         AtomicReference<String> lastInput = new AtomicReference<>();
         AtomicInteger count = new AtomicInteger(0);
 
-        startCodec(defaultEnv, (in) -> {
+        File socket = startCodec(defaultEnv, (in) -> {
             lastInput.set(in.consumeBody((is) -> {
                 try {
                     return IOUtils.toString(is, StandardCharsets.UTF_8);
@@ -167,7 +167,9 @@ public class HTTPStreamCodecTest {
             return OutputEvent.fromBytes(String.format("%d", count.getAndIncrement()).getBytes(), OutputEvent.Status.Success, "text/plain", Headers.emptyHeaders());
         });
 
-        for (int i = 0; i < 5; i++) {
+        HttpClient httpClient = createClient(socket);
+
+        for (int i = 0; i < 100; i++) {
             ContentResponse resp = httpClient.newRequest("http://localhost/call")
               .method("POST")
               .header("Fn-Call-Id", "callID")
@@ -186,21 +188,59 @@ public class HTTPStreamCodecTest {
     }
 
     @Test
+    public void shouldHandleLargeBodies() throws Exception {
+        // Round trips 10 meg of data through the codec and validates it got the right stuff back
+        SecureRandom sr = new SecureRandom();
+        int size = 1024 * 1024 * 10;
+        byte[] randomString = new byte[size];
+        sr.nextBytes(randomString);
+        byte[] inDigest = MessageDigest.getInstance("SHA-256").digest(randomString);
+
+
+        File socket = startCodec(defaultEnv, (in) -> {
+            byte[] content = in.consumeBody((is) -> {
+                try {
+                    return IOUtils.toByteArray(is);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            return OutputEvent.fromBytes(content, OutputEvent.Status.Success, "application/octet-binary", Headers.emptyHeaders());
+        });
+
+        HttpClient client = createClient(socket);
+
+        CompletableFuture<Result> cdl = new CompletableFuture<>();
+        MessageDigest readDigest = MessageDigest.getInstance("SHA-256");
+        defaultRequest(client)
+          .content(new BytesContentProvider(randomString))
+          .onResponseContent((response, byteBuffer) -> {
+              readDigest.update(byteBuffer);
+          })
+          .send(cdl::complete);
+        Result r = cdl.get();
+        assertThat(r.getResponse().getStatus()).isEqualTo(200);
+        assertThat(readDigest.digest()).isEqualTo(inDigest);
+    }
+
+    @Test
     public void shouldConvertStatusResponses() throws Exception {
 
         for (OutputEvent.Status s : OutputEvent.Status.values()) {
             CompletableFuture<InputEvent> lastEvent = new CompletableFuture<>();
 
-            startCodec(defaultEnv, (in) -> {
+            File socket = startCodec(defaultEnv, (in) -> {
                 lastEvent.complete(in);
                 return OutputEvent.fromBytes("hello".getBytes(), s, "text/plain", Headers.emptyHeaders());
             });
 
-            ContentResponse resp = defaultRequest().send();
+            HttpClient client = createClient(socket);
+
+            ContentResponse resp = defaultRequest(client).send();
 
             assertThat(resp.getStatus()).isEqualTo(s.getCode());
 
-            cleanup();
         }
 
     }
@@ -215,23 +255,21 @@ public class HTTPStreamCodecTest {
         }) {
             CompletableFuture<InputEvent> lastEvent = new CompletableFuture<>();
 
-            startCodec(defaultEnv, (in) -> {
+            File socket = startCodec(defaultEnv, (in) -> {
                 lastEvent.complete(in);
                 return OutputEvent.fromBytes("hello".getBytes(), OutputEvent.Status.Success, "text/plain", Headers.emptyHeaders().addHeader(header[0], header[1]));
             });
-
-            ContentResponse resp = defaultRequest().send();
+            HttpClient client = createClient(socket);
+            ContentResponse resp = defaultRequest(client).send();
 
             assertThat(resp.getHeaders().get(header[0])).isNull();
 
-            cleanup();
         }
     }
 
     @Test
     public void socketShouldHaveCorrectPermissions() throws Exception {
-        startCodec(defaultEnv, (in) -> OutputEvent.fromBytes("hello".getBytes(), OutputEvent.Status.Success, "text/plain", Headers.emptyHeaders()));
-        File listener = new File(fileName);
+        File listener = startCodec(defaultEnv, (in) -> OutputEvent.fromBytes("hello".getBytes(), OutputEvent.Status.Success, "text/plain", Headers.emptyHeaders()));
 
         assertThat(Files.getPosixFilePermissions(listener.toPath())).isEqualTo(PosixFilePermissions.fromString("rw-rw-rw-"));
 
