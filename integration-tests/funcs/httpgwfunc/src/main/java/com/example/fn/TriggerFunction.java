6ae96aa5f7e2f4570fd2f9c3fad45eb46f5b9f2f
Owen Cliffe <owen.cliffe@oracle.com>
2018-09-20 22:37:08 +0100

    integration tests

--- integration-tests/funcs/httpgwfunc/src/main/java/com/example/fn/TriggerFunction.java
+++ integration-tests/funcs/httpgwfunc/src/main/java/com/example/fn/TriggerFunction.java
@@ -6,19 +6,19 @@ import com.fnproject.fn.api.httpgateway.HTTPGatewayContext;
 public class TriggerFunction {
 
     public String handleRequest(String input, HTTPGatewayContext hctx, InvocationContext ctx) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
+        String name = (input == null || input.isEmpty()) ? "world" : input;
 
 
+        ctx.setResponseHeader("MyRawHeader", "bar");
+        ctx.addResponseHeader("MyRawHeader", "bob");
 
-        ctx.setResponseHeader("MyRawHeader","bar");
-        ctx.addResponseHeader("MyRawHeader","bob");
 
+        hctx.setResponseHeader("Content-Type", "text/plain");
+        hctx.addResponseHeader("MyHTTPHeader", "foo");
 
-        hctx.setResponseHeader("Content-Type","text/plain");
-        hctx.addResponseHeader("MyHTTPHeader","foo");
-
-        hctx.addResponseHeader("GotMethod",hctx.getMethod());
-        hctx.addResponseHeader("GotURL",hctx.getRequestURL());
+        hctx.addResponseHeader("GotMethod", hctx.getMethod());
+        hctx.addResponseHeader("GotURL", hctx.getRequestURL());
+        hctx.addResponseHeader("GotHeader", hctx.getHeaders().get("Foo").orElse("nope"));
 
         hctx.setStatusCode(202);
         return "Hello, " + name + "!";
