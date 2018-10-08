c99fce75b2fd1791c0ca14695a014cf43debbb30
Owen Cliffe <owen.cliffe@oracle.com>
2018-10-08 16:38:23 +0100

    fix docker build

--- images/init-native/docker-build.sh
+++ images/init-native/docker-build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-if [ -z "${release_version}" ]
-    then
-    release_version=$(cat ../../release.version)
+if [ -z "${FN_FDK_VERSION}" ];  then
+    FN_FDK_VERSION=$(cat ../../release.version)
 fi
-BUILD_VERSION=${FN_FDK_VERSION:-1.0.0-SNAPSHOT}
-sed -i.bak -e "s|<fdk\\.version>.*</fdk\\.version>|<fdk.version>${release_version}</fdk.version>|" pom.xml && rm pom.xml.bak
+sed -i.bak -e "s|<fdk\\.version>.*</fdk\\.version>|<fdk.version>${FN_FDK_VERSION}</fdk.version>|" pom.xml && rm pom.xml.bak
 docker build -t fnproject/fn-java-native-init:${FN_FDK_VERSION} -f Dockerfile-init-image .
