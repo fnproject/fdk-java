327ca84093ae5d53dcca9679f61e5e949b6c6139
Tomas Zezula <tomas.zezula@oracle.com>
2018-11-15 20:50:17 +0100

    [164] Support http-stream protocol in substrate/native java.

--- images/build-native/docker-build.sh
+++ images/build-native/docker-build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ -z "$1" ]
+then
+    echo "Needs runtime folder as an argument"
+    exit 1
+fi
 native_version=$(cat native.version)
 set -e
 
@@ -7,6 +12,10 @@ if docker pull ${native_image} ; then
    echo ${native_image} already exists, skipping native build
    exit 0
 fi
-
-docker build -t "fnproject/fn-java-native:${native_version}" .
+workdir=${1}
+dockerfiledir=$(pwd)
+(
+  cd ${workdir}
+  docker build -f ${dockerfiledir}/Dockerfile -t "fnproject/fn-java-native:${native_version}" .
+)
 echo "fnproject/fn-java-native:${native_version}" > native_build.image
